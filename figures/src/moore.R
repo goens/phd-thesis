f870525bbbcfd5e846a8ba4704d14a0895021136
Andrés Goens <andres.goens@tu-dresden.de>
2020-09-17 15:27:28 +0200

    prettify a bit plot on microprocessor trends¨

--- figures/src/moore.R
+++ figures/src/moore.R
@@ -5,10 +5,10 @@ moore <- read_csv("data/moore.csv")
 
 
 moore_transistors <- subset(moore,!is.na(transistors)& !is.na(frequency) & !is.na(cores) & !is.na(specint))
-moore_transistors$panel <- 'Transistors'
+moore_transistors$panel <- 'Number of Transistors'
 
 moore_specint <- subset(moore,!is.na(specint)& !is.na(frequency) & !is.na(cores) & !is.na(transistors))
-moore_specint$panel <- 'Single-core performance'
+moore_specint$panel <- "Single-core performance\n (SpecINT $\\times 10^3$)"
 
 
 tikz("generated/moore.tex",width=7,height=4)
@@ -19,6 +19,7 @@ ggplot(data=moore) +
   geom_point(data = moore_specint,mapping = aes(x=year,y=specint,size = cores, color = frequency), stat = 'identity') +
   scale_y_log10() +
   scale_radius(trans='log2') +
-  scale_color_viridis_c(trans='log2')
+  scale_color_viridis_c(trans='log2') +
+  labs(x="Year produced", y = element_blank(),color="Frequency\n (MHz)", size="Number\n of cores")
 )
 dev.off()
