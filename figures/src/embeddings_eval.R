92b96ab8b48053f269a81fd2a0cdf019f99faff1
Andres Goens <goens@goenshome>
2021-01-11 15:00:06 +0100

    added simple vector results to metrics + raster plots + hack for creating the proper includes in generated plots

--- figures/src/embeddings_eval.R
+++ figures/src/embeddings_eval.R
@@ -4,6 +4,7 @@ library(tikzDevice)
 library(tsne)
 library(scales)
 library(rsq)
+library(ggrastr)
 
 rankQuantileCut <- function(a,n){
     a_ranks <- rank(a, ties.method = "first")
@@ -44,8 +45,10 @@ vectorized_rel_runtime <- Vectorize(rel_runtime,vectorize.args=c('r1','r2'))
 
 if( !file.exists("data/metrics_rel_distances.csv")){
 mappings_exynos <- read_csv("data/randoms-metrics-exynos.csv")
-#mappings_coolidge <- read_csv("data/randoms-metrics-exynos.csv")
-pruned <- select(mappings_exynos,platform,mapping,representation,representation.target_distortion,representation.extra_dimensions,runtime) 
+mappings_coolidge <- read_csv("data/randoms-metrics-exynos.csv")
+mappings_simplevector <- read_csv("data/random-metrics-simplevector.csv")
+mappings <- full_join(mappings_exynos,full_join(mappings_coolidge,mappings_simplevector))
+pruned <- select(mappings,platform,mapping,representation,representation.target_distortion,representation.extra_dimensions,runtime) 
 pruned$representation <- fct_relevel(pruned$representation, "SimpleVector", "MetricSpaceEmbedding", "SymmetryEmbedding")
 left <- rename(pruned,m1 = mapping,r1 = runtime)
 right <- rename(pruned,m2 = mapping,r2 = runtime)
@@ -60,6 +63,7 @@ write.csv(product,"data/metrics_rel_distances.csv")
 } else{
  product <- read.csv("data/metrics_rel_distances.csv") 
 }
+product$representation <- fct_relevel(product$representation, "SimpleVector", "MetricSpaceEmbedding", "SymmetryEmbedding")
 
 
 increased <- mutate(product, rel_distance = ifelse(representation=="SimpleVector",rel_distance,rel_distance*1))
@@ -69,11 +73,26 @@ increased_scenarios <- increased  %>%
   distinct()
 increased_scenarios$scenario <- fct_relevel(increased_scenarios$scenario,'MetricSpaceEmbedding\nNo-ED', 'SymmetryEmbedding\nNo-ED', 'MetricSpaceEmbedding\nED', 'SymmetryEmbedding\nED','SimpleVector\n')
 
-tikz(file="generated/metric_spaces_comparison.tex",standAlone = F,width=12,height=4)
+tikz(file="generated/metric_spaces_comparison_exynos.tex",standAlone = F,width=12,height=4)
 print(
-  sample_n(increased_scenarios,200) %>% #to make it easier on latex
+  filter(increased_scenarios,platform == 'exynos') %>%
+  #sample_n(min(200,length(rel_runtime))) %>% #to make it easier on latex
   ggplot(mapping = aes(x=rel_distance,y=rel_runtime,color=factor(representation.target_distortion))) +
-  geom_point() +
+  geom_point_rast() +
+  facet_wrap(~scenario,scale='free') +
+  scale_color_brewer(palette = "Greens",na.value="grey50",name="(target)\ndistortion") +
+  labs(x="relative distance",y="relative runtime")  +
+  theme(text=element_text(size=18),
+        legend.position = c(0.8,0.2)) 
+  
+)
+dev.off()
+tikz(file="generated/metric_spaces_comparison_coolidge.tex",standAlone = F,width=12,height=4)
+print(
+  filter(increased_scenarios,platform == 'mppa_coolidge') %>%
+  #sample_n(min(200,length(rel_runtime))) %>% #to make it easier on latex
+  ggplot(mapping = aes(x=rel_distance,y=rel_runtime,color=factor(representation.target_distortion))) +
+  geom_point_rast() +
   facet_wrap(~scenario,scale='free') +
   scale_color_brewer(palette = "Greens",na.value="grey50",name="(target)\ndistortion") +
   labs(x="relative distance",y="relative runtime")  +
@@ -84,6 +103,7 @@ print(
 dev.off()
 only_max <- increased_scenarios %>%
   group_by(representation,
+           platform,
            representation.target_distortion,
            representation.extra_dimensions) %>%
   arrange(rel_distance) %>%
@@ -93,19 +113,35 @@ only_max <- increased_scenarios %>%
   ungroup() %>%
   group_by(representation,
            scenario,
+           platform,
            representation.target_distortion,
            representation.extra_dimensions,
            rel_distance) %>%
   summarize(max_rel_runtime = max(max_rel_runtime)) %>%
   mutate(representation.target_distortion = factor(representation.target_distortion))
 
-tikz(file="generated/metric_spaces_comparison_max.tex",standAlone = F,width=12,height=4)
+tikz(file="generated/metric_spaces_comparison_max_exynos.tex",standAlone = F,width=12,height=4)
 print(
-sample_n(only_max,50) %>%
+  filter(only_max,platform == 'exynos') %>%
+#sample_n(min(50,length(rel_runtime))) %>% #to make it easier on latex
 ggplot(mapping = aes(x=rel_distance,y=max_rel_runtime,color=representation.target_distortion)) +
   scale_color_brewer(palette = "Greens",na.value="grey50",name="(target)\ndistortion") +
   geom_smooth(formula=y~x,method = 'lm') +
-  geom_point() +
+  geom_point_rast() +
+  labs(x="relative distance",y="relative runtime") +
+  facet_wrap(platform~scenario,scale="free") + 
+  theme(text=element_text(size=18),
+        legend.position = c(0.8,0.2)) 
+)
+dev.off()
+tikz(file="generated/metric_spaces_comparison_max_coolidge.tex",standAlone = F,width=12,height=4)
+print(
+filter(only_max,platform == 'mppa_coolidge') %>%
+#sample_n(min(50,length(rel_runtime))) %>% #to make it easier on latex
+ggplot(mapping = aes(x=rel_distance,y=max_rel_runtime,color=representation.target_distortion)) +
+  scale_color_brewer(palette = "Greens",na.value="grey50",name="(target)\ndistortion") +
+  geom_smooth(formula=y~x,method = 'lm') +
+  geom_point_rast() +
   labs(x="relative distance",y="relative runtime") +
   facet_wrap(~scenario,scale="free") + 
   theme(text=element_text(size=18),
@@ -113,14 +149,16 @@ ggplot(mapping = aes(x=rel_distance,y=max_rel_runtime,color=representation.targe
 )
 dev.off()
 
-metrics_max <- group_by(only_max,representation.target_distortion,representation.extra_dimensions,representation) %>%
+metrics_max <- group_by(only_max,representation.target_distortion,representation.extra_dimensions,representation,platform) %>%
   summarize(
     rsq_val = rsq(lm(rel_distance~max_rel_runtime)),
     rho = cor.test(rel_distance,max_rel_runtime, method="spearman")$statistic,
     rho = cor.test(rel_distance,max_rel_runtime, method="spearman")$p.value,
   ) %>% mutate(scenario=paste(representation,ifelse(is.na(representation.extra_dimensions),'',ifelse(representation.extra_dimensions,'ED','No-ED')),sep='\n'))
-metrics_max$scenario <- fct_relevel(metrics_max$scenario,'MetricSpaceEmbedding\nNo-ED', 'SymmetryEmbedding\nNo-ED', 'MetricSpaceEmbedding\nED', 'SymmetryEmbedding\nED','SimpleVector\n')
 
+metrics_max$scenario <- fct_relevel(metrics_max$scenario,'MetricSpaceEmbedding\nNo-ED', 'SymmetryEmbedding\nNo-ED', 'MetricSpaceEmbedding\nED', 'SymmetryEmbedding\nED','SimpleVector\n')
+embedding_labels <- c('MetricSpaceEmbedding\nNo-ED' = 'M.S.Emb.\nNo-ED', 'SymmetryEmbedding\nNo-ED' = 'Sym.+Emb.\nNo-ED', 'MetricSpaceEmbedding\nED'='M.S.Emb.\nED', 'SymmetryEmbedding\nED'='Sym.+Emb.\nED','SimpleVector\n'='Simp. Vec.\n')
+platform_labels <- c('exynos' = "Odroid XU3", 'mppa_coolidge' = "MPPA3 Coolidge")
 tikz("generated/metrics_regression_rsq.tex",width=8,height=3.5,standAlone = F)
 print(
 ggplot(data = metrics_max) +
@@ -129,9 +167,11 @@ ggplot(data = metrics_max) +
            mapping = aes(x=scenario,
                          y=rsq_val,fill=representation.target_distortion)) +
   scale_fill_brewer(palette = "Greens",na.value="grey50",name = "(target)\ndistortion") +
-  theme(axis.text.x = element_text(angle=25,hjust=0.7,vjust=0.4),
+  theme(axis.text.x = element_text(angle=90,hjust=0.7,vjust=0.4),
         text=element_text(size=18),
         axis.ticks.x = element_blank()) +
-  labs(x=element_blank(),y="Linear Regression $R^2$") 
- ) 
+  scale_x_discrete(labels=embedding_labels) +
+  labs(x=element_blank(),y="Linear Regression $R^2$")  +
+  facet_wrap(~platform,labeller = labeller(platform=platform_labels))
+ )  
 dev.off()
